8a71396f9f1da87a5d75a4adf6683dadf5d998aa
arpithaangya <74758263+arpithaangya@users.noreply.github.com>
2023-11-08 09:38:52 +0530

    Delete com.Arpitha.TechAssessment directory

--- com.Arpitha.TechAssessment/src/test/java/RunnerPack/RunnerTestNg.java
+++
@@ -1,8 +0,0 @@
-package RunnerPack;
-
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(features="C:\\Users\\ashekharangya\\Assessment3_Selenium_Arpitha\\com.Arpitha.TechAssessment\\src\\test\\resources\\features\\Register.feature",glue="com.Arpitha.TechAssessment")
-public class RunnerTestNg {
-
-}
